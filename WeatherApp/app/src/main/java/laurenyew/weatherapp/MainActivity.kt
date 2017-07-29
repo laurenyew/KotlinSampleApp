0f392bf75a1934ba1ca07e536d02a9d948c068c3
Lauren Yew <laurenyew@gmail.com>
2017-07-28 21:27:01 -0500

    Completed chp 4-5: Create recycler view forecast text list in kotlin

--- WeatherApp/app/src/main/java/laurenyew/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/laurenyew/weatherapp/MainActivity.kt
@@ -2,13 +2,28 @@ package laurenyew.weatherapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    private val items = listOf<String>(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tues 6/24 - Foggy - 21/8",
+            "Weds 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Setup the list view
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin!"
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 }
