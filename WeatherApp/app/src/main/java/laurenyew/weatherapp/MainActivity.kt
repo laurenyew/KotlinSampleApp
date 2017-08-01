d66190458e8a4d5f8d5de32c3a73bb0bb1fc79b8
Lauren Yew <laurenyew@gmail.com>
2017-07-31 23:10:30 -0500

    Parsed GSON, created Command interface and UI models

--- WeatherApp/app/src/main/java/laurenyew/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/laurenyew/weatherapp/MainActivity.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import laurenyew.weatherapp.requests.ForecastRequest
-import org.jetbrains.anko.*
+import laurenyew.weatherapp.requests.RequestForecastCommand
+import org.jetbrains.anko.custom.async
+import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-
-    private val items = listOf<String>(
-            "Mon 6/23 - Sunny - 31/17",
-            "Tues 6/24 - Foggy - 21/8",
-            "Weds 6/25 - Cloudy - 22/17",
-            "Thurs 6/26 - Rainy - 18/11",
-            "Fri 6/27 - Foggy - 21/10",
-            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    )
+//Deprecated
+//    private val items = listOf<String>(
+//            "Mon 6/23 - Sunny - 31/17",
+//            "Tues 6/24 - Foggy - 21/8",
+//            "Weds 6/25 - Cloudy - 22/17",
+//            "Thurs 6/26 - Rainy - 18/11",
+//            "Fri 6/27 - Foggy - 21/10",
+//            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+//            "Sun 6/29 - Sunny - 20/7"
+//    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,13 +30,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter = ForecastListAdapter(items)
 
         toast("Hello World!")
 
-        doAsync {
-            ForecastRequest("78759").execute()
-            uiThread { longToast("Request performed") }
+        async {
+            val result = RequestForecastCommand("78759").execute()
+            uiThread {
+                forecastList.adapter = ForecastListAdapter(result)
+            }
         }
     }
 }
