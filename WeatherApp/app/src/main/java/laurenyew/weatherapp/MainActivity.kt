d36d0ba16365be46a36c3e78612155a4d0f945ab
Lauren Yew <laurenyew@gmail.com>
2017-07-31 22:44:49 -0500

    Working with data classes and url requests

--- WeatherApp/app/src/main/java/laurenyew/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/laurenyew/weatherapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
+import laurenyew.weatherapp.requests.ForecastRequest
 import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,14 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         toast("Hello World!")
 
-        val APP_ID = "5c6984500a430a71931b8baf146d0c10"
-        val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?"
-        val QUERY = "q=78759&mode=json&units=metric&cnt=7"
-        val COMPLETE_URL = "$URL" + "APPID=$APP_ID&$QUERY"
-
-        Log.d(javaClass.simpleName, "Query: $COMPLETE_URL")
         doAsync {
-            Request(COMPLETE_URL).run()
+            ForecastRequest("78759").execute()
             uiThread { longToast("Request performed") }
         }
     }
