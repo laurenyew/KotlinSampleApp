d357ca45cc931cacd26c465820406135ca62b337
Lauren Yew <laurenyew@gmail.com>
2017-09-24 18:16:08 -0500

    Updating Weather app to latest versions of kotlin / gradle

--- WeatherApp/app/src/main/java/laurenyew/weatherapp/ui/ForecastListAdapter.kt
+++ WeatherApp/app/src/main/java/laurenyew/weatherapp/ui/ForecastListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import laurenyew.weatherapp.R
 import laurenyew.weatherapp.model.Forecast
 import laurenyew.weatherapp.model.ForecastList
